15898cbb8e8b60ca64ab2db1c8c6bf819d70dfef
NehaNafishaDas <nehadas7044@gmail.com>
2019-12-30 04:05:03 -0500

    Add files via upload

--- InventoryManagement/src/com/sid/Constants.java
+++ InventoryManagement/src/com/sid/Constants.java
@@ -3,11 +3,14 @@ package com.sid;
 public class Constants {
 	
 	public static final int INT_ZERO=0;
+	public static final int ONE=1;
+	public static final int TWO=2;
+	public static final int THREE=3;
 	public static final double DOUBLE_ZERO=0.00;
 	
 	public static final boolean FALSE=false;
 	public static final boolean TRUE=true;
-	public static final String TITLE="\t\t IVENTORY REPORT";
+	public static final String TITLE="\t\t INVENTORY REPORT";
 	
 	public static final String PATTERN="%9s %9s %7s %12s %7s";
 	
@@ -26,5 +29,15 @@ public class Constants {
 	
 	public static final String VALUE_MSG="Total Value: \t\t\t\t";
 	public static final String PROFIT_MSG="Profit since previous report: \t\t";
+	
+	public static final String SPACE=" ";
+	
+	public static final String CREATE="create";
+	public static final String UPDATE_SELL="updateSell";
+	public static final String UPDATE_BUY="updateBuy";
+	public static final String UPDATE_SELL_PRICE="updateSellPrice";
+	public static final String DELETE="delete";
+	public static final String REPORT="report";
+	public static final String WRONG_COMMAND="Wrong Command";
 	
 }
--- InventoryManagement/src/com/sid/Controller.java
+++ InventoryManagement/src/com/sid/Controller.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Controller {
+public class Controller extends Constants {
 	
 	static List<Item> itemsList =  new ArrayList<Item>();
 
@@ -18,40 +18,40 @@ public class Controller {
 		String line;
 		while ((line = in.readLine()) != null) {
 			String input  = line;
-			String[] str = input.split(" ");
-			String command =str[0];
+			String[] str = input.split(SPACE);
+			String command =str[INT_ZERO];
 			OperationsImpl op = new OperationsImpl();
 			switch(command) {
-			case "create": 
-				String item = str[1];
-				double cp = Double.parseDouble(str[2]);
-				double sp = Double.parseDouble(str[3]);
+			case CREATE: 
+				String item = str[ONE];
+				double cp = Double.parseDouble(str[TWO]);
+				double sp = Double.parseDouble(str[THREE]);
 				op.create(item, cp, sp);
 				break;
-			case "updateSell": 
-				item = str[1];
-				int soldQuantity = Integer.parseInt(str[2]);
+			case UPDATE_SELL: 
+				item = str[ONE];
+				int soldQuantity = Integer.parseInt(str[TWO]);
 				op.updateSell(item, soldQuantity);
 				break;
-			case "updateBuy" : 
-				item = str[1];
-				int totalQuantity = Integer.parseInt(str[2]);
+			case UPDATE_BUY : 
+				item = str[ONE];
+				int totalQuantity = Integer.parseInt(str[TWO]);
 				op.updateBuy(item, totalQuantity);
 				break;
-			case "updateSellPrice" : 
-				item = str[1];
-				double sellingPrice = Double.parseDouble(str[2]);
+			case UPDATE_SELL_PRICE : 
+				item = str[ONE];
+				double sellingPrice = Double.parseDouble(str[TWO]);
 				op.updateSellPrice(item, sellingPrice);
 				break;
-			case "delete" : 
-				item = str[1];
+			case DELETE : 
+				item = str[ONE];
 				op.delete(item);
 				break;
-			case "report" : 
+			case REPORT : 
 				op.report();
 				break;
 			default: 
-				System.out.println("Wrong command");
+				System.out.println(WRONG_COMMAND);
 			}
 		}
 	}
